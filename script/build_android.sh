ee81cfa60d43ce997bbfdf6271b00d29fa078a64
zengsj <zengshuangjie@gmail.com>
2019-06-12 08:47:04 +0800

    fix build script

--- script/build_android.sh
+++ script/build_android.sh
@@ -1,37 +1,25 @@
 #!/bin/bash
 
-gn gen out/android_x86/ --args="import(\"//yealink/build/args/android.gni\") target_cpu=\"x86\" target_os=\"android\" is_debug=false"
-gn gen out/android_x86_debug/ --args="import(\"//yealink/build/args/android.gni\") target_cpu=\"x86\" target_os=\"android\" is_debug=true"
+gn gen out/android_x86/ --args="import(\"//yealink/build/args/all.gni\") target_cpu=\"x86\" target_os=\"android\" is_debug=false"
+gn gen out/android_x86_debug/ --args="import(\"//yealink/build/args/all.gni\") target_cpu=\"x86\" target_os=\"android\" is_debug=true"
 
-gn gen out/android_x64/ --args="import(\"//yealink/build/args/android.gni\") target_cpu=\"x64\" target_os=\"android\" is_debug=false"
-gn gen out/android_x64_debug --args="import(\"//yealink/build/args/android.gni\") target_cpu=\"x64\" target_os=\"android\" is_debug=true"
+gn gen out/android_x64/ --args="import(\"//yealink/build/args/all.gni\") target_cpu=\"x64\" target_os=\"android\" is_debug=false"
+gn gen out/android_x64_debug --args="import(\"//yealink/build/args/all.gni\") target_cpu=\"x64\" target_os=\"android\" is_debug=true"
 
-gn gen out/x86 --args="import(\"//yealink/build/args/android.gni\") target_cpu=\"x86\" is_debug=false"
-gn gen out/x86_debug --args="import(\"//yealink/build/args/android.gni\") target_cpu=\"x86\" is_debug=true"
+gn gen out/android_arm --args="import(\"//yealink/build/args/all.gni\") target_cpu=\"arm\" target_os=\"android\" is_debug=false"
+gn gen out/android_arm_debug --args="import(\"//yealink/build/args/all.gni\") target_cpu=\"arm\" target_os=\"android\" is_debug=true"
 
-gn gen out/x64 --args="import(\"//yealink/build/args/android.gni\") target_cpu=\"x64\" is_debug=false"
-gn gen out/x64_debug --args="import(\"//yealink/build/args/android.gni\") target_cpu=\"x64\" is_debug=true"
+gn gen out/android_arm64 --args="import(\"//yealink/build/args/all.gni\") target_cpu=\"arm64\" target_os=\"android\" is_debug=false"
+gn gen out/android_arm64_debug --args="import(\"//yealink/build/args/all.gni\") target_cpu=\"arm64\" target_os=\"android\" is_debug=true"
 
-gn gen out/android_arm --args="import(\"//yealink/build/args/android.gni\") target_cpu=\"arm\" target_os=\"android\" is_debug=false"
-gn gen out/android_arm_debug --args="import(\"//yealink/build/args/android.gni\") target_cpu=\"arm\" target_os=\"android\" is_debug=true"
+ninja -C out/android_x86/ yealink
+ninja -C out/android_x86_debug/ yealink
 
-gn gen out/android_arm64 --args="import(\"//yealink/build/args/android.gni\") target_cpu=\"arm64\" target_os=\"android\" is_debug=false"
-gn gen out/android_arm64_debug --args="import(\"//yealink/build/args/android.gni\") target_cpu=\"arm64\" target_os=\"android\" is_debug=true"
+ninja -C out/android_x64/ yealink
+ninja -C out/android_x64_debug/ yealink
 
-ninja -C out/android_x86/
-ninja -C out/android_x86_debug/
+ninja -C out/android_arm/ yealink
+ninja -C out/android_arm_debug/ yealink
 
-ninja -C out/android_x64/
-ninja -C out/android_x64_debug/
-
-ninja -C out/x86/
-ninja -C out/x86_debug/
-
-ninja -C out/x64/
-ninja -C out/x64_debug/
-
-ninja -C out/android_arm/
-ninja -C out/android_arm_debug/
-
-ninja -C out/android_arm64/
-ninja -C out/android_arm64_debug/
+ninja -C out/android_arm64/ yealink
+ninja -C out/android_arm64_debug/ yealink
